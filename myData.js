873baaa6e1ee63c3f202fc0c306ca9f532f0a1cf
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-10-13 06:14:13 +0530

    v0.1 - release

---
+++ myData.js
@@ -0,0 +1,33 @@
+module.exports = {
+  packageName: "moni",
+  name: {
+    first: "Soumyadip",
+    last: "Moni",
+  },
+  siteUrl: "https://soumyadipmoni.netlify.app",
+  jobDescription: "Full Stack TypeScript && Rust Developer",
+  location: "Bhubaneswar, India",
+  description: `I have been building large scale websites for more than 5 years. I have worked with many technologies and frameworks. I have a passion for learning and sharing my knowledge with others in a way that's as public as possible. I love open source and I love blogging.`,
+  userLinks: [
+    {
+      label: "Instagram",
+      url: "https://instagram.com/soumyadip_moni",
+    },
+    {
+      label: "Twitter",
+      url: "https://twitter.com/Avater004",
+    },
+    {
+      label: "LinkedIn",
+      url: "https://www.linkedin.com/in/soumyadip-moni/",
+    },
+    {
+      label: "GitHub",
+      url: "https://github.com/AvaterClasher",
+    },
+    {
+      label: "Email",
+      url: "avater.clasher@gmail.com",
+    },
+  ],
+};
---
+++ readme.md
@@ -0,0 +1,8 @@
+# NPM Contact Card
+
+![version](https://img.shields.io/npm/v/smoni.svg?style=flat-square)
+
+My take on the NPX me contact card.
+
+To run - `npx smoni`
+
